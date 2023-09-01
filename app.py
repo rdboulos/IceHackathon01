7cc2b42fe8f3f5807673e677b3503e6d249d1cdd
rdboulos <89005126+rdboulos@users.noreply.github.com>
2023-09-01 11:54:50 +1000

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ if "openai_key" in st.session_state:
             df = pd.read_csv(uploaded_file)
             st.session_state.df = df
             
-            data_frames = []
-            data_frames.append(df)
-            combined_df = pd.concat(data_frames, ignore_index=True)
+            #data_frames = []
+            #data_frames.append(df)
+            #combined_df = pd.concat(data_frames, ignore_index=True)
 
     with st.form("Question"):
         question = st.text_input("Question", value="", type="default")
@@ -42,7 +42,7 @@ if "openai_key" in st.session_state:
         if submitted:
             with st.spinner():
                 llm = OpenAI(api_token=API_KEY_2)
-                pandas_ai = PandasAI(llm, conversational=False, enable_cache=False)
+                pandas_ai = PandasAI(llm, conversational=True, enable_cache=False)
                 x = pandas_ai.run(st.session_state.df, prompt=question)
                 
                 #fig = plt.gcf()
